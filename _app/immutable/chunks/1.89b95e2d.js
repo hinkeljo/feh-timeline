f758e545ffd0b80be748b857ee361a8feba4311b
hinkeljo <hinkeljonathan@protonmail.com>
2023-05-07 14:09:22 +0200

    Updates

--- _app/immutable/chunks/1.89b95e2d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.0236d4c1.js";export{t as component};
---
+++ _app/immutable/chunks/2.a20fe497.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.88f6e443.js";export{t as component};
